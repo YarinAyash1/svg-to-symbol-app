f4b7e2c185a01227c8eb9c8f527367d8c55f9612
Yarin Ayash <yarinayyash@gmail.com>
2023-03-10 18:59:48 +0200

    on mount create request for up server

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import {ResultsProvider} from "./context.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import Results from "./components/Results.jsx";
 import {Col, Container, Row} from "react-bootstrap";
+import {useEffect} from "react";
+import axios from "axios";
 function App() {
+
+    useEffect(() => {
+        axios.get(`${import.meta.env['VITE_API_URL']}/keep-alive`)
+    }, [])
+
     return (
         <ResultsProvider>
             <Container className={'mt-3 mb-3'}>
